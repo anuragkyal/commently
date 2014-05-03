d83b707bef589279cb62aff2b6f9cd82300913c0
suvodhoy <suvodhoy.sinha@bankbazaar.com>
2014-05-03 15:20:11 +0530

    fb integration, post & fetch comments
    Signed-off-by: suvodhoy <suvodhoy.sinha@bankbazaar.com>

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,107 @@
  * Created by anurag on 3/5/14.
  */
 
+function loadFacebookSDK() {
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '652275434852148', // app id as present in the fb developer dashboard.
+            status     : true, // check login status
+            cookie     : true, // enable cookies to allow the server to access the session
+            xfbml      : true,  // parse XFBML
+            oauth      : true
+        });
+    };
+
+    //Load the SDK asynchronously
+    (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
+}
+
+function loadProfileFromFacebook(response) {
+    FB.api('/me?fields=name,email', function(response) {
+        if (($("[name='reviewer.email']").val() == undefined || $("[name='reviewer.email']").val() == '')) {
+            $("[name='reviewer.email']").attr('value', response.email);
+            $("[name='reviewer.firstName']").attr('value', response.first_name);
+            $("[name='reviewer.lastName']").attr('value', response.last_name);
+            $("[name='reviewer.password']").attr('value', response.id);
+            $("[name='reviewer.customerType']").attr('value', 'FACEBOOK');
+        }
+    });
+}
+
+function getLoginStatus() {
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            loadProfileFromFacebook(response);
+            return "SUCCESS";
+        } else {
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    FB.api('/me/permissions', function(response) {
+                    });
+                    loadProfileFromFacebook(response);
+                    return "SUCCESS";
+                }
+            }, {scope:'email, user_about_me, user_birthday, user_location'});
+        }
+
+    });
+}
+
 $(function(){
     init();
 });
 
 
 function init(){
-
+    loadFacebookSDK();
 }
 
-function getUrl(){
-
+function getCurrentURL(){
+    encodeURIComponent($(location).attr('href').split("//")[1]);
 }
 
+/*function getSelectedText() {
+ $('#showSelected').on('click', function(){
 
+ var text = "";
+ if (window.getSelection) {
+ text = $.getSelection().toString();
+ } else if (document.selection && document.selection.type != "Control") {
+ text = document.selection.createRange().text;
+ }
 
+ alert(text);
+ });
+ }*/
 
+function fetchComments(url) {
+    // calling the api
+    $.ajax({
+        cache:false,
+        url: "0.0.0.0:5080/getComments" + url,
+        type:'post',
+        success : function(response) {
+            return response;
+        }
+    });
+}
+
+function postComment() {
+    if (getLoginStatus() == "SUCCESS") {
+        $.ajax({
+            cache:false,
+            url: "0.0.0.0:5080/postComments",
+            type:'post',
+            data: {url : getCurrentURL(), comment : ""/*pick from selector*/, "user" : ""/*pick from selector*/},
+            success : function(response) {
+                return response;
+            }
+        });
+    }
+}
